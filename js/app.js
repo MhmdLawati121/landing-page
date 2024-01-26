a0254b15d6b52fc471339f72d26a8410e1a8d5c8
mhmdLawati121 <mhmdlawati121@gmail.com>
2024-01-26 17:01:21 +0400

    Documentation Added

--- js/app.js
+++ js/app.js
@@ -17,10 +17,27 @@
  * Global Variables
  *
  */
+
+// Number of sections in the document - change this value if number of sections change
 const number_of_sections = 4;
+
+// Array to store elements
 const sections = [];
+
+// Navigation bar element
 const nav_bar = document.getElementById("navbar__list");
+
+// Array to store navigation bar anchor elements
 const anchors = [];
+
+// HTMLCollection to store all section headings
+const section_titles = document.getElementsByTagName("h2");
+
+// Creating list of all sections
+for (let i = 1; i <= number_of_sections; i++) {
+  const section = document.getElementById("section" + i);
+  sections.push(section);
+}
 
 /**
  * End Global Variables
@@ -28,16 +45,21 @@ const anchors = [];
  * Begin Main Functions
  */
 
-// build the nav
+/**
+ * @description Creates a navigation bar dynamically
+ * @param {HTMLCollection} headings
+ */
 function buildNav(headings) {
   for (let item of headings) {
     const section_id = item.parentElement.parentElement.id;
+
+    // Creating list and anchor elements
     const list_item = document.createElement("li");
     const list_anchor = document.createElement("a");
 
+    // Setting anchor content and id (id set to include section name for easier reference)
     list_anchor.textContent = item.textContent;
     list_anchor.classList.add("menu__link");
-    //list_anchor.href = "#" + section_id;
     list_anchor.id = "a" + section_id;
 
     anchors.push(list_anchor);
@@ -47,17 +69,15 @@ function buildNav(headings) {
   }
 }
 
-// Creating list of all sections
-for (let i = 1; i <= number_of_sections; i++) {
-  const section = document.getElementById("section" + i);
-  sections.push(section);
-}
-
-// Detecting scrolling
+/**
+ * @description Add active classes to nav-bar anchor and section when entering viewport
+ */
 function activateArea() {
   for (let section of sections) {
     const rect = section.getBoundingClientRect();
     const menu = document.getElementById("a" + section.id);
+
+    // Check if in viewport and near the top
     if (rect.top <= 100 && rect.bottom >= 100) {
       section.classList.add("your-active-class");
       menu.classList.add("active");
@@ -74,8 +94,7 @@ function activateArea() {
  *
  */
 
-// Build menu
-const section_titles = document.getElementsByTagName("h2");
+// Build navbar when DOM is loaded
 document.addEventListener("DOMContentLoaded", buildNav(section_titles));
 
 // Scroll to section on link click
